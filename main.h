0d7abc09b2bc38c11ce8ef858b11b4177faca978
greg0rys <shenefeltgregory@gmail.com>
2022-09-27 17:47:07 -0700

    moving this to main branch

--- main.h
+++ main.h
@@ -3,6 +3,11 @@
 // being lazy dropping the use of the "std" namesapce
 using namespace std;
 const int NUMBER = 20;
+class person
+{
+	string name;
+	int age;
+};
 
 int main();
 void sayHello(string name);
